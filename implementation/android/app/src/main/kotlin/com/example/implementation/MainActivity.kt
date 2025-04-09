1f8613cc907458ee2b7f3f83dcc22ff3a7c9a1a1
Himaza Zahara <152014521+himaza007@users.noreply.github.com>
2025-04-09 23:46:36 +0530

    CHANGES 7

--- implementation/android/app/src/main/kotlin/com/example/implementation/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.implementation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ physioconnect/android/app/src/main/kotlin/com/example/physioconnect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.physioconnect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
